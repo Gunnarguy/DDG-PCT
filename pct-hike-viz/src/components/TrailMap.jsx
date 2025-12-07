65e5aa259082bc56100eb76398f96566f008826a
gunnarguy <gunnarguy@me.com>
2025-12-06 18:23:50 -0800

    fix: force explicit map dimensions for mobile Safari

--- pct-hike-viz/src/components/TrailMap.jsx
+++ pct-hike-viz/src/components/TrailMap.jsx
@@ -125,7 +125,7 @@ function TrailMap({
           zoom: 8
         }}
         mapStyle={mapStyles[selectedStyle].url}
-        style={{ width: '100%', height: '100%' }}
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
         attributionControl
       >
         <DeckOverlay layers={deckLayers} />
